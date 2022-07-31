965e9b84827a08b11975787635aa2a8386098050
Galih Agus Setyawan <galihdevelopment@gmail.com>
2022-07-31 22:13:25 +0700

    create responsive story page

--- pages/story/index.js
+++ pages/story/index.js
@@ -1,25 +1,112 @@
 import Head from "next/head";
+import dynamic from "next/dynamic";
+
 // import components
 import Header from '../../components/header.component';
-import Footer from '../../components/footer.component';
+import Image from "next/image";
+const Footer = dynamic(() => import('../../components/footer.component'));
+
+const contentList = [
+    {
+        title: 'STOP EFEK NEGATIF PENGGUNAAN SKINCARE',
+        desc: 'Masih banyak orang yang menggunakan hydroquinone dan steroid sebagai alternatif Pertama untuk mencerahkan kulit dan untuk mengobati flek/ hiperpigmentasi kulit. Namun hydroquinone dan steroid hanya bisa digunakan jika dalam pengawasan dokter. Karena efek negatif kedepannya yang akan menimbulkan masalah baru pada kulit. LEUTI Perfect Sublimate Serum efek cerah dan putihnya tak kalah dengan penggunaan hydroquinone dan steroid. Yang tentunya LEUTI Perfect Sublimate Serum lebih aman digunakan dalam jangka waktu yang sangat lama.',
+        image: '/images4.jpg'
+    },
+    {
+        title: 'STOP BUANG WAKTU & UANG',
+        desc: 'Jaman sekarang ini para wanita karir makin sibuk, namun disisi lain mereka dituntut untuk semakin tampil lebih baik. Begitu juga dengan penampilan kulit wajah. Maraknya sosial media, keeksisan mereka di sosial media mengharuskan penampilan dan kulit cantik terawat ditengah kesibukan yang semakin padat. Tak jarang mereka harus menjalani serangkaian skincare yang tak dapat dilewatkan untuk tetap menjaga kulit wajah mereka tetap terawat. /n LEUTI Perfect Sublimate Serum akan membantu melewatkan beberapa produk biasa yang mereka pakai. Pemakaian night cream pencerahan, serum pencerahan, serum anti ageing, masker rutin, beberapa Dari rangkaian tersebut dapat dilewatkan dan cukup diganti dengan LEUTI Perfect Sublimate Serum. Akan lebih menghemat waktu dan uang dengan tanpa mengurangi keefektifitasannya.',
+        image: '/images4.jpg'
+    },
+    {
+        title: '5 RANGKAIN SKINCARE DALAM 1 PRODUK',
+        desc: 'LEUTI Perfect Sublimate Serum mampu menggantikan 5 rangkaian skincare anda untuk mendapatkan kulit wajah yang lebih putih, cerah, flawless, halus dan kenyal.',
+        image: '/images4.jpg'
+    },
+    {
+        title: 'KEMASAN DAN KUALITAS',
+        desc: 'Pengemasan akhir LEUTI Perfect Sublimate Serum secara manual sehingga formula di dalamnya tetap terjaga dan tidak rusak karena suhu tinggi saat pengemasan. LEUTI Perfect Sublimate Serum 15 ml namun cukup membuat para wanita ingin melihat cermin terus menerus, ingin menyentuh kulit wajah karena kenyal dan halus seperti sedang menyentuh agar-agar.',
+        image: '/images3.jpeg'
+    }
+];
 
 export default function Story() {
     return (
         <>
             <Head></Head>
             <Header />
-            <main className=" md:w-4/5 m-auto">
+            <main className=" md:w-4/5 m-auto flex flex-col md:gap-28 gap-14 md:px-0 px-5">
 
                 {/* banner */}
-                <div className=" md:w-full md:h-[600px] md:grid md:grid-cols-2">
-                    <div className=" md:w-full md:h-full bg-green-200"></div>
-                    <div className=" md:w-full md:h-full bg-fuchsia-400"></div>
-                    <div className="md:absolute md:top-1/2 md:left-1/2 md:transform md:-translate-x-1/2 md:-translate-y-1/2">
-                        <h1 className="md:text-center">THE STORY</h1>
+                <div className=" md:w-full md:h-[600px] md:grid md:grid-cols-2 md:bg-gray-100">
+                    <div className=" md:w-full md:h-full relative">
+                        <Image
+                            src={'/images2.jpg'}
+                            layout='fill'
+                            objectFit='cover'
+                            objectPosition='center'
+                        />
+                    </div>
+                    <div className=" md:w-full md:h-full relative">
+                        <Image
+                            src={'/images4.jpg'}
+                            layout='fill'
+                            objectFit='cover'
+                            objectPosition='center'
+                        />
+                    </div>
+                    <div className="md:flex md:flex-col md:gap-3 text-center md:absolute md:top-1/2 md:left-1/2 md:transform md:-translate-x-1/2 md:-translate-y-1/2">
+                        <h1 className="md:text-center md:text-lg text-2xl">THE STORY</h1>
                         <h2 className="md:text-center md:text-4xl">LEUTI PERFECT SUBLIMATE SERUM</h2>
                     </div>
                 </div>
                 {/* end of banner */}
+
+                <div className="md:w-1/2 flex flex-col md:gap-10 gap-5">
+                    <h3 className="md:text-xl text-lg">LEUTI STORIES</h3>
+                    <p className=" leading-relaxed">LEUTI Perfect Sublimate Serum diciptakan bagi orang-orang yang menuntut kesempurnaan dan kualitas tinggi. Sebelum menyentuh pasar, serum ini telah melewati uji sampling keefektifitasan dimana 98% dari 100 orang dengan kulit normal sebagai sampling menunjukkan hasil yang memuaskan dalam jangka waktu maksimal 2 minggu. 80% dari mereka mengatakan bahwa hasil yang bagus dirasakan sejak pertama kali pemakaian serum LEUTI Perfect Sublimate Serum. Efek pada kulit dimana kulit menjadi lebih putih, flawless, halus, lembab, kenyal seperti sedang menyentuh agar-agar dirasakan secara bersamaan. </p>
+                </div>
+
+                <div className="md:w-full flex justify-center">
+                    <div className="md:w-52 md:h-48 w-60 h-52 relative">
+                        <Image
+                            src={'/full-logo.jpeg'}
+                            quality={50}
+                            loading='lazy'
+                            layout='fill'
+                            objectFit='contain'
+                        />
+                    </div>
+                </div>
+
+                <div className="md:w-1/2 md:font-semibold md:text-lg font-semibold">
+                    <p>ALASAN DIBALIK LAHIRNYA LEUTI PERFECT SUBLIMATE SERUM YANGE SEMPURNA UNTUK MEMENUHI KEBUTUHAN SEMUA SKINCARE ANDA</p>
+                </div>
+
+                <div className="md:grid md:grid-cols-2 md:gap-5">
+
+                    {
+                        contentList.map((data, index) => {
+                            return (
+                                <div key={index} className="flex flex-col gap-5 md:mt-10 mt-20">
+                                    <div className="md:w-full md:h-[700px] w-full h-96 relative bg-gray-100">
+                                        <Image
+                                            src={data.image}
+                                            loading='lazy'
+                                            layout='fill'
+                                            objectPosition='center'
+                                            objectFit='cover'
+                                        />
+                                    </div>
+                                    <div className="flex flex-col gap-5">
+                                        <h3 className="md:font-semibold md:text-lg font-semibold">{data.title}</h3>
+                                        <p className=" leading-relaxed">{data.desc.split('/n')}</p>
+                                    </div>
+                                </div>
+                            );
+                        })
+                    }
+
+                </div>
 
             </main>
             <Footer />
